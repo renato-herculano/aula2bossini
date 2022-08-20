4a16c24ea5130d406a690a951de619648709be21
bianca <biancakrafecik@hotmail.com>
2022-08-19 21:54:08 -0300

    aula 2 bossini

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
+  const obterTextoDoBotao = () => "Enviar";
+  const divStyle = {margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8, outline: 'none'};
+  const labelStyle = {display: 'block', marginBottom: 4};
+  const label = 'Nome:'
+  const inputStyle = {paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', width: '100%', borderRadius: 8, outline: 'none'};
+  const buttonStyle = {marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', color: 'white', border: 'none', borderRadius: 8, width: '100%'};
   return (
-    <div>
-
+    <div style={divStyle}>
+      <label htmlFor="nome" style={labelStyle}>{label}</label>
+      <input type="text" id='nome' style={inputStyle}/>
+      <button style={buttonStyle}>{obterTextoDoBotao()}</button>    
     </div>
   )
 }
